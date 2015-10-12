431726f9804ebc0cc602524cbc6a83d9aebb9101
ArturN <arturnista@gmail.com>
2015-10-12 18:14:20 -0300

    All

---
+++ Assets/Cell/Cell.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	public bool alive;
+
+	private SpriteRenderer sprite;
+	private bool nextAlive;
+	private Cell[] neighborsCell;
+
+	void Awake(){
+		sprite = GetComponentInChildren<SpriteRenderer> ();
+		Cell[] cellsInGame = GameObject.FindObjectsOfType<Cell> ();
+		neighborsCell = new Cell[8];
+		int numbCell = 0;
+		foreach (Cell thisCell in cellsInGame) {
+			float posX = transform.position.x - thisCell.transform.position.x;
+			float posY = transform.position.y - thisCell.transform.position.y;
+			// If it's the same cell
+			if(posX == 0 & posY == 0){
+				continue;
+			}
+			if(posX > 1 || posX < -1){
+				continue;
+			}
+			if(posY > 1 || posY < -1){
+				continue;
+			}
+			neighborsCell[numbCell++] = thisCell;
+		}
+	}
+	
+	public bool IsAlive(){
+		return alive;
+	}
+
+	public void Step(){
+		if (nextAlive) {
+			ReviveCell ();
+		} else {
+			KillCell();
+		}
+	}
+
+	public void PrepareStep(){
+		int counter = CountNumberNeighbors ();
+		if (alive) {
+			if(counter < 2){
+				nextAlive = false;
+			} else if(counter > 3){
+				nextAlive = false;
+			} else {
+				nextAlive = true;
+			}
+		} else {
+			if(counter == 3){
+				nextAlive = true;
+			}
+		}
+	}
+
+	int CountNumberNeighbors(){
+		int counter = 0;
+		foreach (Cell cell in neighborsCell) {
+			if(cell == null){
+				continue;
+			}
+			if(!cell.IsAlive()){
+				continue;
+			}
+			counter++;
+		}
+		return counter;
+	}
+
+	public void KillCell(){
+		alive = false;
+		nextAlive = false;
+		sprite.color = Color.black;
+	}
+
+	public void ReviveCell(){
+		alive = true;
+		nextAlive = true;
+		sprite.color = Color.red;
+	}
+
+	void OnMouseDown(){
+		if (alive) {
+			KillCell ();
+		} else {
+			ReviveCell();
+		}
+	}
+}
---
+++ Assets/GameController/GameController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private float stepTime;
+	private bool running;
+	private float timeCounter;
+	private Slider stepTimeSlider;
+	private Text buttonStartStopText;
+	Cell[] cellsInGame;
+
+	void Start(){
+		cellsInGame = GameObject.FindObjectsOfType<Cell> ();
+		Clear ();
+		stepTimeSlider = GameObject.Find ("StepTime Slider").GetComponent<Slider>();
+		stepTimeSlider.value = stepTimeSlider.maxValue / 2;
+		buttonStartStopText = GameObject.Find ("Start/Stop Button").GetComponentInChildren<Text> ();
+		buttonStartStopText.text = "Run";
+		running = false;
+	}
+
+	public void ChangeState(){
+		running = !running;
+		if (running) {
+			buttonStartStopText.text = "Stop";
+		} else {
+			buttonStartStopText.text = "Run";
+		}
+	}
+
+	public void ChangeStepTime(){
+		stepTime = 10f / stepTimeSlider.value;
+	}
+
+	public void Clear(){
+		foreach (Cell cell in cellsInGame) {
+			cell.KillCell();
+		}
+	}
+
+	public void Fill(){
+		foreach (Cell cell in cellsInGame) {
+			cell.ReviveCell();
+		}
+	}
+
+	public void RandomPad(){
+		foreach (Cell cell in cellsInGame) {
+			if(Random.Range(0f, 10f) > 7){
+				cell.ReviveCell();
+			} else {
+				cell.KillCell();
+			}
+		}
+	}
+
+	void Update () {
+		if (!running) {
+			return;
+		}
+		timeCounter += Time.deltaTime;
+		if (timeCounter > stepTime) {
+			timeCounter = 0;
+			foreach (Cell cell in cellsInGame) {
+				cell.PrepareStep();
+			}
+			foreach (Cell cell in cellsInGame) {
+				cell.Step();
+			}
+		}
+	}
+}
